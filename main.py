3ee389d658d50e24dad58dcbdd65f0288b7ef971
wlord01 <wlord01@gmail.com>
2017-10-11 13:36:48 +0200

    pixelLoop moved into Retina class inside __init__.py

--- main.py
+++ main.py
@@ -45,33 +45,6 @@ from shapely.geometry import Point as poi
 from polygonobjects import Polygon, Circle, Hand, Retina
 
 plt.ion()
-###############################################################_________METHODS       
-def pixelLoop(grid,tri,squ,cir,retiPixVec):
-    cell=-1 #pixel
-    for pix in (grid): #for every pixel of the retina's grid --->
-        cell+=1
-        
-        #___check if PIXEL is on a FIGURE, then: 
-                                          #   add the value RGB in retinaVector
-                                          #   depending on which is the figure 
-        
-        #----->below..
-        if (poly(tri).intersects(poly(pix)))==True:
-            retiPixVec[0,cell]=green
-        elif (poly(squ).intersects(poly(pix)))==True:
-            retiPixVec[0,cell]=red
-        elif ((cir).intersects(poly(pix))) == True:
-            retiPixVec[0,cell]=blue
-        else:
-            retiPixVec[0,cell]=others #   if pixel doesn't overlapping figures
-        #.....below again :P    
-        #N.B. (poly .... intersects) is a function of "shapely" library; it allows
-        #to check if two figures intersecates in 3 ways:
-        #   1. True if two figures edges are in touch
-        #   2. True if one figure overlap the other
-        #   3. True if one figure is inside the other
-        #So it works even if only one point of the figure in on one of the other.
-        #*we should consider to use it instead of checkPoint and checkPath! ;)
       
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%            
 #%%%%%%%%%%%%%%%%%%%%_________________________________%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -152,7 +125,7 @@ ext_pixelGrid=ext_objReti.pixelCalc() #calculates list of all pixels coordinates
 #-----> see below 
 ext_circle=poi(ext_figPosList[cir][0]).buffer(cirRad) 
 
-pixelLoop(ext_pixelGrid,ext_objTri.xy,ext_objSqu.xy,ext_circle,ext_retiPixVec)               
+Retina.pixelLoop(ext_pixelGrid,ext_objTri.xy,ext_objSqu.xy,ext_circle,ext_retiPixVec)               
 ext_retiPixMat=np.flipud(np.reshape(ext_retiPixVec, (ext_pixelsX,ext_pixelsY))) #   MATRIX 
                                                             #   of pixels color
                                                             #np.flipud is necessary to not have the matrix reversed ;)
@@ -212,7 +185,7 @@ int_pixelGrid=ext_pixelGrid #or int_objReti.pixelCalc() #calculates list of all 
 #-----> see below 
 int_circle=poi(int_objCir.xy).buffer(cirRad) 
 
-pixelLoop(int_pixelGrid,int_objTri.xy,int_objSqu.xy,int_circle,int_retiPixVec)
+Retina.pixelLoop(int_pixelGrid,int_objTri.xy,int_objSqu.xy,int_circle,int_retiPixVec)
            
 int_retiPixMat=np.flipud(np.reshape(int_retiPixVec, (int_pixelsX,int_pixelsY))) #   MATRIX 
                                                         #   of pixels color
@@ -294,7 +267,7 @@ while numTri < maxTri and ((poi(objHand.xy).within(poly(ext_objTri.xy))) != True
     ext_retiPixALL=np.vstack((ext_retiPixALL,np.matrix(np.zeros(ext_pixelsX*ext_pixelsY))))
     ext_retiPixVec=ext_retiPixALL[numTri,:]
 
-    pixelLoop(ext_pixelGrid,ext_objTri.xy,ext_objSqu.xy,ext_circle,ext_retiPixVec)       
+    Retina.pixelLoop(ext_pixelGrid,ext_objTri.xy,ext_objSqu.xy,ext_circle,ext_retiPixVec)       
     ext_retiPixMat=np.flipud(np.reshape(ext_retiPixVec, (ext_pixelsX,ext_pixelsY))) #   MATRIX 
                                                             #   of pixels color
 #as above!
@@ -304,7 +277,7 @@ while numTri < maxTri and ((poi(objHand.xy).within(poly(ext_objTri.xy))) != True
     int_retiPixALL=np.vstack((int_retiPixALL,np.matrix(np.zeros(int_pixelsX*int_pixelsY))))
     int_retiPixVec=int_retiPixALL[numTri,:]
 
-    pixelLoop(int_pixelGrid,int_objTri.xy,int_objSqu.xy,int_circle,int_retiPixVec)       
+    Retina.pixelLoop(int_pixelGrid,int_objTri.xy,int_objSqu.xy,int_circle,int_retiPixVec)       
     int_retiPixMat=np.flipud(np.reshape(int_retiPixVec, (int_pixelsX,int_pixelsY))) #   MATRIX 
                                                             #   of pixels color    
     
