af6ecc52c7161783debbf0b228d7a8690fbd6e65
wlord01 <wlord01@gmail.com>
2017-10-31 12:08:53 +0100

    Update of Square class with new attributes and methods. Also docstring update.

--- polygonobjects/__init__.py
+++ polygonobjects/__init__.py
@@ -1,31 +1,80 @@
 #!/usr/bin/env python
 
-"""A collection of polygon objects created with shapely library"""
+"""A collection of geometrical objects.
+
+Function:
+math_round(x)
+
+Classes:
+Polygon
+Square
+Circle
+Retina
+"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as ptc
 
 
+def math_round(x):
+    """Round to integer
+
+    Round arithmetically to nearest integer, with ties going away from zero.
+    """
+    if abs(x) < 1:
+        if x >= 0.5:
+            x = 1
+        else:
+            x = 0
+    else:
+        if x % int(x) >= 0.5:
+            x = int(x) + 1
+        else:
+            x = int(x)
+    return x
+
+
 class Polygon(object):
-    """Polygon"""
-    def __init__(self,xy): #CONSTRUCTOR
-        self.xy=xy
-        self.color="none"
-        self.linestyle="-"
-        self.edge="k"
-        
-    def randPos(self): #random position in x/y limits
-        randomX=np.random.uniform(0,10)
-        randomY=np.random.uniform(0,10)
-        randomXY=np.array([randomX,randomY])
+    """Polygon object
+
+    Variables:
+    - Type (string?)
+    - Color (integer? RGB code?)
+    - Center coordinates (floats)
+    - Size (integer? Or float?)
+    - Coordinates of object with respect to center
+
+    Methods:
+    - Move object
+    - Draw on table
+    - Tell if point is inside
+    """
+    type_ = "Polygon"
+
+    def __init__(self, center, size, color, unit):
+        self.center = center
+        self.size = size
+        self.color = color
+        self.unit = unit
+
+        # Old stuff
+        self.linestyle = "-"
+        self.edge = "k"
+
+    def randPos(self):  # Old stuff
+        randomX = np.random.uniform(0, 10)
+        randomY = np.random.uniform(0, 10)
+        randomXY = np.array([randomX, randomY])
         return randomXY
-        
-    def plotDef(self): #define the plot
-        self.figure=ptc.Polygon(self.xy,facecolor = self.color, 
-                                linestyle=self.linestyle, edgecolor=self.edge)
-                 
-    def plotAdd(self,numFig,title): #add plot to figure
+
+    def plotDef(self):  # Old stuff
+        self.figure = ptc.Polygon(self.xy, facecolor=self.color,
+                                  linestyle=self.linestyle,
+                                  edgecolor=self.edge
+                                  )
+
+    def plotAdd(self,numFig,title): # Old stuff
         self.xLim=(-5,15)      
         self.yLim=(-5,15)
         self.fig=plt.figure(numFig) 
@@ -36,12 +85,76 @@ class Polygon(object):
         
         self.ax1.add_patch(self.figure)
         
-    def animateFig(self,coorList,position,numFig,title): #plot animation
+    def animateFig(self,coorList,position,numFig,title): # Old stuff
         self.xy=coorList[position]
         
         self.figure.remove()
         self.plotDef()
         self.plotAdd(numFig,title)
+
+
+class Square(Polygon):
+    """Square object with inheritance from Polygon class.
+
+    Variables:
+    - Type (string)
+    - Color (integer 0/1 or RGB list)
+    - Center coordinates (floats)
+    - Size (float)
+
+    Methods:
+    - Move object
+    - Draw on table
+    - Tell if point is inside
+    """
+    type_ = "Square"
+
+    def move(self, vector):
+        """Move object by adding vector to its center position."""
+        self.center += vector
+
+    def get_corners(self):
+        """Get the corner coordinates of the square."""
+        _center = self.center*(self.unit-1)  # Pixel-value
+        _size = self.size*self.unit
+        _x_min = math_round(_center[0] - _size/2)
+        _x_max = math_round(_center[0] + _size/2)
+        _y_min = math_round(_center[1] - _size/2)
+        _y_max = math_round(_center[1] + _size/2)
+        _corner_coordinates = [np.array([_x_min, _x_max], dtype=int),
+                               np.array([_y_min, _y_max], dtype=int)
+                               ]
+        return _corner_coordinates
+
+    def draw(self, grid):
+        """Draw object in grid.
+
+        Convert position and size to array index values and update grid.
+
+        Keyword arguments:
+        grid -- the grid to draw in
+        unit_measure -- the size of the grid
+        """
+        _corner_coordinates = self.get_corners()
+        grid[_corner_coordinates[1][0]:_corner_coordinates[1][1],
+             _corner_coordinates[0][0]:_corner_coordinates[0][1]
+             ] = self.color
+
+    def is_inside(self, point):
+        """Check if point is inside square.
+
+        If x-coordinate of point is between x_min and x_max of square and
+        y-coordinate of point is between y_min and _ymax of square, return
+        True. Otherwise return False.
+        """
+        _x_min = self.center[0] - self.size/2
+        _x_max = self.center[0] + self.size/2
+        _y_min = self.center[1] - self.size/2
+        _y_max = self.center[1] + self.size/2
+        if _x_min <= point[0] <= _x_max and _y_min <= point[1] <= _y_max:
+            return True
+        else:
+            return False
 
 
 class Circle(Polygon):
@@ -147,3 +260,41 @@ class Retina(Polygon):
             #   3. True if one figure is inside the other
             #So it works even if only one point of the figure in on one of the other.
             #*we should consider to use it instead of checkPoint and checkPath! ;)
+
+if __name__ == '__main__':
+    # Run tests
+    plt.close('all')
+
+    external_size = 1024
+
+    # Put grid as ones for RGB images
+#    external_grid = np.ones([external_size, external_size, 3])
+    external_grid = np.zeros([external_size, external_size])
+
+    p1 = Square(np.array([0.5, 0.5], dtype=float), 0.2, 1, external_size)
+
+    # Before square is put in
+#    plt.imshow(external_grid)
+#    plt.show()
+
+    plt.figure()
+    print(external_grid)  # Before square is put in
+    p1.draw(external_grid)
+    plt.imshow(external_grid)
+    print(external_grid)  # After square is put in
+
+    # Test points for is_inside()
+    point = np.array([0.53, 0.457])
+    print(p1.is_inside(point))
+    plt_point = point*(external_size-1)
+    plt.plot(plt_point[0], plt_point[1], 'wo')
+#
+#    p1.move(np.array([0.1, 0.1]))
+#    p1.draw(external_grid)
+#    plt.imshow(external_grid)
+#
+#    p1.move(np.array([-0.1, 0.1]))
+#    p1.draw(external_grid)
+#    plt.imshow(external_grid)
+    
+    plt.show()
--- simulation.py
+++ simulation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Main simulation script
+Main simulation script.
 
 Here the main simulation of the system is run. Graphics are plotted for
 the simulation if desired. This should be decided by button-clicks. One
@@ -11,11 +11,12 @@ discrete steps in the simulation.
 
 import numpy as np
 
+
 def new_random_position(max_values):
     """
-    Returns coordinates randomly chosen within limits
-    
-    Takes as input a list of dimensions of table object to make sure
+    Return coordinates randomly chosen within limits.
+
+    Take as input a list of dimensions of table object to make sure
     the new position is inside the working environment.
     """
     random_values = []
@@ -23,32 +24,35 @@ def new_random_position(max_values):
         random_values.append(np.random.uniform(0, max_value))
     return np.array(random_values)
 
+
 def move(object_, end_position):
     """
-    Moves object from current position to end_position
+    Move object from current position to end_position.
 
-    What about the hand? And fovea? Needs to be incorporated here. 
-    Also, this should be implemented as a paremeterised skill later
+    What about the hand? And fovea? Needs to be incorporated here.
+    Also, this should be implemented as a paremeterised skill later.
     """
     object_.xy = end_position
 
+
 def is_inside(fovea_coordinates, polygon):
     """
-    Check if coordinates are inside object
-    
+    Check if coordinates are inside object.
+
     Uses shapely object function 'within'. Only checks for fovea in
     polygon for now, but could probably be generalised.
     Takes input xy-coordinates and polygon object.
-    Returns True/False.
+    Return True/False.
     """
-    return poi(fovea_coordinates).within(poly(polygon.xy))
+    pass
+
 
 def sub_goal(fovea_coordinates, polygons):
     """
     Check if current position of fovea is on a sub-goal.
 
     If the fovea is inside a polygon a sub-goal is found.
-    
+
     # PSEUDO-CODE (NOT FINISHED)
     _found = False
     FOR all polygons
@@ -73,12 +77,13 @@ def sub_goal(fovea_coordinates, polygons):
     else:
         return False
 
+
 def goal_accomplished_classifier(internal_retina_matrix,
                                  external_retina_matrix, threshold
                                  ):
     """
     Compare the internal and external retina to see if goal is
-    accomplished
+    accomplished.
 
     Takes two numpy arrays and compares the normalised distance between
     their flattened versions. The retina arrays should be 2D. The
@@ -86,8 +91,8 @@ def goal_accomplished_classifier(internal_retina_matrix,
     flattened vectors are similar enough (external image close enough
     to internal goal image).
     """
-    internal_retina_vector = internal_retina_matrix.flatten() # Row-wise
-    external_retina_vector = external_retina_matrix.flatten() # Row-wise
+    internal_retina_vector = internal_retina_matrix.flatten()  # Row-wise
+    external_retina_vector = external_retina_matrix.flatten()  # Row-wise
     diff = internal_retina_vector - external_retina_vector
     norm = np.linalg.norm(diff)
     if norm/len(internal_retina_vector) <= threshold:
@@ -95,9 +100,10 @@ def goal_accomplished_classifier(internal_retina_matrix,
     else:
         return False
 
+
 def goal_achievable_classifier():
     """
-    Checks if goal can be achieved by parameterised skill from current
+    Check if goal can be achieved by parameterised skill from current
     position.
 
     This is something that comes later with the parameterised skill
@@ -108,25 +114,42 @@ def goal_achievable_classifier():
     """
     return
 
-def parameterised_skill():
+
+def parameterised_skill(ext_fov, int_fov):
     """
-    Moves object in external environment to match internal goal. 
+    Move object in external environment.
 
     This parameterised skill has to learn. This will be the main part of
     the intelligent system. We will implement this using RBM.
 
     FOR NOW: Implement hardwired moving of objects.
+
+    Arguments:
+    - External fovea position
+    - Internal fovea position
+
+    Return vector between external and internal fovea position
     """
-    return
+    pass
+
+
+def move_object(object, vector):
+    """Move the objects center along the vector.
+
+    Needs to know limits so object is not moved outside table.
+    """
+    pass
+
 
 def foveate(retina):
     """
-    Foveation of retina
+    Foveate retina.
 
     Uses the {RGB --> Black/White --> Add noise --> (smooth) -->
-    foveate } procedure.
+    foveate} procedure.
     """
     return
+
 
 def main():
     """
@@ -139,8 +162,7 @@ def main():
     max_search_steps = integer
     number_of_steps = integer
     graphics_on = True/False    # Update graphics each step or not
-    discrete_steps_on = True/False    # Whether the system should take discrete steps
-
+    discrete_steps_on = True/False
 
     # COUNTERS
     step
@@ -158,7 +180,7 @@ def main():
             FUNCTION foveate(internal_retina) moves internal retina
             FUNCTION sub_goal() checks if sub_goal_found
             # MAYBE THIS BELOW SHOULD BE OUTSIDE ANYWAY? JUST CHECK EXTERNAL
-            # ENVIRONMENT IF A SUB-GOAL IS FOUND IN INTERNAL ENVIRONMENT? 
+            # ENVIRONMENT IF A SUB-GOAL IS FOUND IN INTERNAL ENVIRONMENT?
             SET external_retina position to match internal_retina position
             FUNCTION goal_accomplished_classifier() checks if
                 sub_goal_accomplished
@@ -182,6 +204,7 @@ def main():
     """
     # SET VARIABLES
     number_of_steps = 10
+    sub_goal_found = False
 
     # MAIN FUNCTIONING
     for step in range(1, number_of_steps+1):
