d5831a790197888cb3dc9586031d79657df4dc44
wlord01 <wlord01@gmail.com>
2017-11-06 17:23:28 +0100

    Update of sub_goal(), parameterised_skill(), move_object() and foveate().

--- simulation.py
+++ simulation.py
@@ -10,7 +10,9 @@ discrete steps in the simulation.
 """
 
 import numpy as np
-from polygonobjects import Square
+import matplotlib.pyplot as plt
+
+from geometricshapes import Square, Circle, Retina
 
 
 def new_random_position(max_values):
@@ -46,25 +48,26 @@ def sub_goal(fovea_coordinates, polygons):
     If the fovea is inside a polygon a sub-goal is found.
 
     # PSEUDO-CODE (NOT FINISHED)
-    _found = False
+    found = False
     FOR all polygons
-        IF not _found
+        IF not found
             FUNCTION is_inside(fovea_coordinates, polygon) checks if
                 fovea is inside the polygon
             IF fovea is inside polygon
-                SET _found = True
-    IF _found
+                SET found = True
+    IF found
         return True
     ELSE
         return False
     """
-    _found = False
+    found = False
     for polygon in polygons:
-        if not _found:
+        if not found:
             if is_inside(fovea_coordinates, polygon):
-                _found = True
+                found = True
+                return polygon
 
-    if _found:
+    if found:
         return True
     else:
         return False
@@ -107,7 +110,7 @@ def goal_achievable_classifier():
     return
 
 
-def parameterised_skill(ext_fov, int_fov):
+def parameterised_skill(ext_fov, int_fov, object_):
     """
     Move object in external environment.
 
@@ -117,20 +120,22 @@ def parameterised_skill(ext_fov, int_fov):
     FOR NOW: Implement hardwired moving of objects.
 
     Arguments:
-    - External fovea position
-    - Internal fovea position
-
-    Return vector between external and internal fovea position
+    - ext_fov -- array of float coordinates of external fovea
+    - int_fov -- array of float coordinates of internal fovea
+    - object_ -- the object that the function should move
     """
-    pass
+    vector = int_fov - ext_fov
+    move_object(object_, vector)
 
 
-def move_object(object, vector):
+def move_object(object_, vector):
     """Move the objects center along the vector.
 
-    Needs to know limits so object is not moved outside table.
+    Needs to know limits so object is not moved outside table. This
+    check is inside the class method move() now, but maybe it should
+    be out here?
     """
-    pass
+    object_.move(vector)
 
 
 def foveate(retina):
@@ -139,8 +144,10 @@ def foveate(retina):
 
     Uses the {RGB --> Black/White --> Add noise --> (smooth) -->
     foveate} procedure.
+
+    For now: just move retina to random pos.
     """
-    return
+    retina.move(0.8*np.random.random_sample(2) - 0.4)
 
 
 def main():
@@ -197,6 +204,8 @@ def main():
     # SET VARIABLES
     number_of_steps = 10
     sub_goal_found = False
+    sub_goal_accomplished = False
+    sub_goal_achievable = False
 
     # MAIN FUNCTIONING
     for step in range(1, number_of_steps+1):
@@ -213,3 +222,39 @@ if __name__ == '__main__':
     """
     # main()
     # Run tests
+
+    # Set up environment
+    unit = 100
+
+    # INTERNAL
+    int_image = np.ones([unit, unit, 3])
+
+    # Create objects
+    int_c = Circle([0.3, 0.4], 0.15, [1, 0, 0], unit)
+    int_s = Square([0.6, 0.6], 0.1, [0, 0, 1], unit)
+    int_objects = [int_c, int_s]
+
+    int_c.draw(int_image)
+    int_s.draw(int_image)
+
+    # Get retina
+    int_retina = Retina([0.55, 0.55], 0.3, [1, 1, 1], unit)
+    int_ret_image = int_retina.get_retina_image(int_image)
+
+    plt.imshow(int_image)
+    plt.plot([20, 20, 80, 80, 20], [20, 80, 80, 20, 20])
+    ret_indices = int_retina.get_index_values()
+    print(ret_indices)
+    plt.plot([ret_indices[0][0], ret_indices[0][0], ret_indices[0][1],
+              ret_indices[0][1], ret_indices[0][0]],
+             [ret_indices[1][0], ret_indices[1][1], ret_indices[1][1],
+              ret_indices[1][0], ret_indices[1][0]]
+             )
+
+    plt.figure()
+    plt.imshow(int_ret_image)
+    plt.show()
+
+    sub_goal = sub_goal(int_retina.center, int_objects)
+    if sub_goal:
+        print(sub_goal)
