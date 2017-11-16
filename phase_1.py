9b67a202b6ea99f415502cb6c6929e7a789df994
wlord01 <wlord01@gmail.com>
2017-11-16 11:58:13 +0100

    Implement functions in phase_1.py
    - Update update_avg_ignorance and check_action_success.
    
    - Implement check_free_space to avoid overlapping and
      get_random_position to generate random moves.
    
    Ignore the tests in main.

--- phase_1.py
+++ phase_1.py
@@ -49,19 +49,27 @@ FOR step in range number_of_steps
     PERCEPTRON checks the knowledge about the focus image using
         (p.get_output)
     SET ignorance as the absolute distance from the knowledge to 1 or 0
-    IF ignorance > average_ignorance
+    IF ignorance + 0.05 > average_ignorance
+        FUNCTION get_random_position() generates random xy coordinates
+           as target position
+        FUNCTION check_free_space() checks if the chosen target
+            position is free
         FUNCTION parameterised_skill is applied on the object to try to
-            move it to a random goal position  # THIS NEEDS CHECK_OVERLAP
+            move it to the target position
         FUNCTION check_action_success checks if the action was
             successful by comparing the focus image before the action
             and the focus image at the target location after the action
-            # THIS NEEDS TO ADD MASK TO IMAGES
         IF successful_action
             PERCEPTRON is updated (p.update_weights) with target = 1
         IF not successful_action
             PERCEPTRON is updated (p.update_weights) with target = 0
     FUNCTION update_avg_ignorance updates the average ignorance (leaky)
 """
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import simulation as s
 
 
 def step(p):
@@ -73,7 +81,7 @@ def step(p):
     pass
 
 
-def update_avg_ignorance(average_ignorance, object_ignorance, rate=0.5):
+def update_avg_ignorance(average_ignorance, object_ignorance, rate=0.05):
     """Update the average ignorance
 
     Keyword arguments:
@@ -82,15 +90,11 @@ def update_avg_ignorance(average_ignorance, object_ignorance, rate=0.5):
     - rate -- the update rate between 0 and 1 (float)
 
     Update the average ignorance using the formula:
-    I_avg_t = I_avg_t-1 + rate * I_t,
+    I_avg_t = (1 - rate) * I_avg_t-1 + rate * I_t,
     where I_avg_t-1 is the average ignorance before update, rate is the
-    update rate and I_t is the current ignorance of the object in focus
+    leak rate and I_t is the current ignorance of the object in focus.
     """
-    change = -rate*average_ignorance + object_ignorance
-    change = -(1-rate)*average_ignorance + rate*object_ignorance
-    change = -rate*average_ignorance + (1-rate)*object_ignorance
-    average_ignorance += change
-    return average_ignorance
+    return (1-rate)*average_ignorance + rate*object_ignorance
 
 
 def check_action_success(before_image, after_image):
@@ -110,8 +114,70 @@ def check_action_success(before_image, after_image):
     where the object in focus is in the before_image. This mask is then
     transferred to the after_image.
     """
-    pass
+    image_match = s.check_images(before_image, after_image)
+    if image_match:
+        return True
+    else:
+        return False
+
+
+def check_free_space(env_image, target_xy, fovea_size, unit):
+    """Check if target area is free
+
+    Keyword arguments:
+    - env_image -- image array of the environment
+    - target_xy -- the xy coordinates of the target position
+    - fovea_size -- the size of the fovea image
+    - unit -- the unit size/size of environment
+
+    Check if the focus area around the target position enx_xy is free
+    space. The method creates a temporary fovea image at the target
+    position and checks if it is all white.
+
+    Returns True/False.
+    """
+    temp_fovea = s.Retina(target_xy, fovea_size,  [1, 1, 1], unit)
+    temp_image = temp_fovea.get_retina_image(env_image)
+    if np.array_equal(temp_image, np.ones(temp_image.shape)):
+        return True
+    else:
+        return False
+
+
+def get_random_position(limits):
+    """Generate random xy coordinates within limits
+
+    Keyword arguments:
+    - limits -- array of [[x_min, x_max], [y_min, y_max]]
+    """
+    x = (limits[0][1]-limits[0][0])*np.random.random_sample() + limits[0][0]
+    y = (limits[1][1]-limits[1][0])*np.random.random_sample() + limits[1][0]
+    return np.array([x, y])
 
 
 if __name__ == '__main__':
     """Main"""
+#    image_1 = np.zeros((10, 10))
+#    image_2 = np.ones((10, 10))
+#
+#    unit = 100
+#    env, fovea, objects = s.external_env_init(unit)
+#
+#    fovea_im = fovea.get_retina_image(env)
+#    print(check_action_success(fovea_im, fovea_im))
+#
+#    print(check_free_space(env, [0.65, 0.35], fovea.size, unit))
+
+#    plt.imshow(fovea.get_retina_image(env))
+#    plt.show()
+
+    average_ignorance = 0.5
+    object_ignorance = 0.5
+    print(average_ignorance)
+    average_ignorance = update_avg_ignorance(average_ignorance,
+                                             object_ignorance,
+                                             rate=0.05
+                                             )
+    print(average_ignorance)
+    
+    print(get_random_position(np.array([[0.2, 0.8], [0.2, 0.8]])))
